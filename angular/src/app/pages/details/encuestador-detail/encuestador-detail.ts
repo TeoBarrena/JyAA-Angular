93cfeba3cab30b66d7f1911fab3904d5b3d92455
teo barrena <teobarrena@gmail.com>
2025-08-12 19:09:51 -0300

    modifico encuestador detail

--- angular/src/app/pages/details/encuestador-detail/encuestador-detail.ts
+++ angular/src/app/pages/details/encuestador-detail/encuestador-detail.ts
@@ -5,11 +5,12 @@ import { environment } from '../../../../environments/environment.development';
 import { AuthService } from '../../../auth/auth-service/auth-service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { RouterLink, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-encuestador-detail',
-  imports: [Navbar, CommonModule, FormsModule],
+  imports: [Navbar, CommonModule, FormsModule, RouterLink],
   templateUrl: './encuestador-detail.html',
   styleUrl: './encuestador-detail.css'
 })
